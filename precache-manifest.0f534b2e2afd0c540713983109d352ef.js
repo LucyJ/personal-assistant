95a0b8d1dcd6fc9ba0079f8e1b6ee907f59730a6
LucyJ <lucyjiao1215@gmail.com>
2020-01-13 10:08:52 -0500

    Updates

---
+++ precache-manifest.0f534b2e2afd0c540713983109d352ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4454e629c01b74679ccfc4b286fc384c",
+    "url": "/personal-assistant/index.html"
+  },
+  {
+    "revision": "a0e997e7059da8036a8a",
+    "url": "/personal-assistant/static/js/2.8a345396.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/personal-assistant/static/js/2.8a345396.chunk.js.LICENSE"
+  },
+  {
+    "revision": "196270c795f887b3618a",
+    "url": "/personal-assistant/static/js/main.5406763a.chunk.js"
+  },
+  {
+    "revision": "6af68dc79e5ea81f8987",
+    "url": "/personal-assistant/static/js/runtime-main.9f1a1dcf.js"
+  }
+]);
